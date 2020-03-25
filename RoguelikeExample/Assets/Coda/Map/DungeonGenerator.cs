cb676c34a29297a21dddbe1db45c1048f1dd0a09
Maksym Sliadniev <maksim.sliadniev@gmail.com>
2020-03-25 13:00:42 +0200

    *backup

---
+++ RoguelikeExample/Assets/Coda/Map/DungeonGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonGenerator : MonoBehaviour
+{
+    public int m_mapWidth;
+    public int m_mapHeight;
+
+    public int m_roomWidthMin;
+    public int m_roomWidthMax;
+    public int m_roomHeightMin;
+    public int m_roomHeightMax;
+
+    public int m_corridorMaxLength;
+    public int m_maxFeatures;
+
+    public void InitializeDungeon()
+    {
+        MapManager.m_map = new Tile[m_mapWidth, m_mapHeight];
+    }
+
+    private void FirstRoom()
+    {
+        Feature room = new Feature();
+        room.m_positions = new List<Position>();
+    }
+}
---
+++ RoguelikeExample/Assets/Coda/Map/MapManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MapManager
+{
+    public static Tile[,] m_map;
+
+
+}
+
+[Serializable]
+public class Tile
+{
+    /*public int PositionX;
+    public int PositionY;*/
+    public Position m_position;
+
+    [NonSerialized] public GameObject m_baseObject;
+}
+
+[Serializable]
+public class Position
+{
+    public int x;
+    public int y;
+}
+
+[Serializable]
+public class Wall
+{
+    public List<Position> m_positions;
+    public string m_direction;
+    public int m_length;
+    public bool m_hasFearure = false;
+}
+
+[Serializable]
+public class Feature
+{
+    public List<Position> m_positions;
+    public Wall[] m_walls;
+    public string m_type;
+    public int m_width;
+    public int m_height;
+}
